99cdbd98ea92661e21deb3209827d78244c52321
agupta54 <anirudhgupta12@gmail.com>
2018-08-15 19:36:59 +0530

    intervals

--- inference.R
+++ inference.R
@@ -371,3 +371,61 @@ dat <- read.csv("mice_pheno.csv")
 control <- filter(dat, Diet=="chow") %>% select(Bodyweight)
 treatment <- filter(dat, Diet=="hf") %>% select(Bodyweight)
 t.test(treatment,control)
+dat <- read.csv("mice_pheno.csv")
+chowPopulation <- dat[dat$Sex=="F" & dat$Diet=="chow",3]
+mu_chow <- mean(chowPopulation)
+print(mu_chow)
+N <- 30
+chow <- sample(chowPopulation,N)
+print(mean(chow))
+se <- sd(chow)/sqrt(N)
+print(se)
+pnorm(2)-pnorm(-2)
+Q <- qnorm(1 - 0.05/2)
+interval <- c(mean(chow)-Q*se,mean(chow)+Q*se)
+interval
+interval[1] < mu_chow & interval[2] > mu_chow
+B <- 250
+mypar()
+plot(mean(chowPopulation)+c(-7,7),c(1,1),type="n",
+     xlab="weight",ylab="interval",ylim=c(1,B))
+abline(v=mean(chowPopulation))
+for (i in 1:B){
+  chow <- sample(chowPopulation,N)
+  se <- sd(chow)/sqrt(N)
+  interval <- c(mean(chow)-Q*se,mean(chow)+Q*se)
+  covered <- 
+    mean(chowPopulation) <= interval[2] & mean(chowPopulation) >= interval[1]
+  color <- ifelse(covered,1,2)
+  lines(interval,c(i,i),col=color)
+}
+mypar()
+plot(mean(chowPopulation)+c(-7,7),c(1,1),type="n",
+     xlab="weight",ylab="interval",ylim=c(1,B))
+abline(v=mean(chowPopulation))
+Q <- qnorm(1- 0.05/2)
+N <- 5
+for (i in 1:B) {
+  chow <- sample(chowPopulation,N)
+  se <- sd(chow)/sqrt(N)
+  interval <- c(mean(chow)-Q*se, mean(chow)+Q*se)
+  covered <- mean(chowPopulation) <= interval[2] & mean(chowPopulation) >= interval[1]
+  color <- ifelse(covered,1,2)
+  lines(interval, c(i,i),col=color)
+}
+mypar()
+plot(mean(chowPopulation)+c(-7,7),c(1,1),type="n",
+     xlab="weight",ylab="interval",ylim=c(1,B))
+abline(v=mean(chowPopulation))
+Q <- qt(1- 0.05/2, df=4)
+N <- 5
+for (i in 1:B) {
+  chow <- sample(chowPopulation,N)
+  se <- sd(chow)/sqrt(N)
+  interval <- c(mean(chow)-Q*se, mean(chow)+Q*se)
+  covered <- mean(chowPopulation) <= interval[2] & mean(chowPopulation) >= interval[1]
+  color <- ifelse(covered,1,2)
+  lines(interval, c(i,i),col=color)
+}
+qnorm(1-0.05/2)
+qt(1-0.05/2,df=4)
