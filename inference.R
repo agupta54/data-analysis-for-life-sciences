a7f4915cc960928ef673ab5098fa7f440a8a4910
agupta54 <anirudhgupta12@gmail.com>
2018-08-16 21:58:23 +0530

    t-test in action

--- inference.R
+++ inference.R
@@ -429,3 +429,115 @@ for (i in 1:B) {
 }
 qnorm(1-0.05/2)
 qt(1-0.05/2,df=4)
+dat <- read.csv("mice_pheno.csv")
+controlPopulation <- filter(dat, Sex == "F" & Diet == "chow") %>%
+  select(Bodyweight) %>% unlist
+hfPopulation <- filter(dat, Sex == "F" & Diet == "hf") %>%
+  select(Bodyweight) %>% unlist 
+mu_hf <- mean(hfPopulation)
+mu_control <- mean(controlPopulation)
+print(mu_hf - mu_control)
+print((mu_hf-mu_control)/mu_control * 100)
+set.seed(1)
+N <- 5
+hf <- sample(hfPopulation,N)
+control <- sample(controlPopulation,N)
+t.test(hf,control)$p.value
+N <- 12
+alpha <- 0.05
+B <- 2000
+reject <- function(N, alpha=0.05){
+  hf <- sample(hfPopulation,N)
+  control <- sample(controlPopulation,N)
+  pval <- t.test(hf,control)$p.value
+  pval < alpha
+}
+reject(12)
+rejections <- replicate(B, reject(N))
+mean(rejections)
+Ns <- seq(5, 50, 5)
+power <- sapply(Ns,function(N){
+  rejections <- replicate(B,reject(N))
+  mean(rejections)
+})
+plot(Ns, power, type="b")
+N <- 30
+alphas <- c(0.1,0.05,0.01,0.001,0.0001)
+power <- sapply(alphas, function(alpha){
+  rejections <- replicate(B,reject(N,alpha=alpha))
+  mean(rejections)
+})
+plot(alphas,power,log="x")
+
+### Exercises ### 
+babies <- read.table("babies.txt", header = TRUE)
+bwt.nonsmoke <- filter(babies,smoke==0) %>% select(bwt) %>% unlist
+bwt.smoke <- filter(babies, smoke==1) %>% select(bwt) %>% unlist
+library(rafalib)
+mean(bwt.nonsmoke) - mean(bwt.smoke)
+popsd(bwt.nonsmoke)
+popsd(bwt.smoke)
+"1."
+set.seed(1)
+N <- 25
+dat.ns <- sample(bwt.nonsmoke,N) 
+dat.s <- sample(bwt.smoke,N)
+tval <- t.test(dat.ns,dat.s)
+tval
+"2."
+pval <- 1-(pnorm(abs(tval$statistic))-pnorm(-abs(tval$statistic)))
+pval
+tval$p.value
+"3. D"
+"4."
+qnorm(0.995)*sqrt( sd( dat.ns)^2/N + sd( dat.s)^2/N )
+"5."
+set.seed(1)
+qt(0.995,2*N-2)*sqrt( sd( dat.ns)^2/N + sd( dat.s)^2/N )
+"6. C"
+"7. A"
+"8."
+N=5
+set.seed(1)
+dat.ns <- sample(bwt.nonsmoke , N) 
+dat.s <- sample(bwt.smoke , N) 
+t.test(dat.s, dat.ns)$p.value
+"9. B"
+"10."
+N=5
+set.seed(1)
+dat.ns <- sample(bwt.nonsmoke , N) 
+dat.s <- sample(bwt.smoke , N) 
+t.test(dat.s, dat.ns)$p.value>0.05
+N=5
+set.seed(1)
+rejects <- replicate(10000,{
+  dat.ns <- sample(bwt.nonsmoke , N)
+  dat.s <- sample(bwt.smoke , N)
+  t.test(dat.s, dat.ns)$p.value < 0.05
+})
+mean(rejects)
+"11."
+Ns=c(10,60,90,120)
+res <- sapply(Ns, function(N){
+  set.seed(1)
+  rejects <- replicate(10000,{
+    dat.ns <- sample(bwt.nonsmoke , N)
+    dat.s <- sample(bwt.smoke , N)
+    t.test(dat.s, dat.ns)$p.value < 0.05
+  })
+  mean(rejects)
+})
+Ns[ which.min( abs( res - .8) ) ] 
+"12."
+Ns=c(10,60,90,120)
+res <- sapply(Ns, function(N){
+  set.seed(1)
+  rejects <- replicate(10000,{
+    dat.ns <- sample(bwt.nonsmoke , N)
+    dat.s <- sample(bwt.smoke , N)
+    t.test(dat.s, dat.ns)$p.value < 0.01
+  })
+  mean(rejects)
+})
+Ns[ which.min( abs( res - .8) ) ]
